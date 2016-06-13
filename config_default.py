db0e3bd8e6b42bf7551fb5977a6b037a291f7b5d
James Thompson <100stacks@users.noreply.github.com>
2016-06-13 18:52:10 -0500

    WTForms basic form, not wired up yet

---
+++ config_default.py
@@ -0,0 +1,5 @@
+# Application configurations
+# 
+# Add sensitive config vars here... **RENAME** to `config.py`
+# NOTE: `config.py` is ignored through .gitignore
+SECRET_KEY = 'replace-this-string'
